d8d62927ed3a1edcdc4ccfe38ef81a1119831247
Biniam Aregay <seifubiniam6@gmail.com>
2024-11-11 11:45:13 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-center h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">Hello, Tailwind!</h1>
+    </div>
+  );
+};
+
+export default App;
