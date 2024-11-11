b193a91d61e301a69ab15f386c90a4f64611e538
Biniam Aregay <seifubiniam6@gmail.com>
2024-11-11 13:41:50 +0300

    build the navbar, and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import NavBar from "./components/NavBar";
+
 const App = () => {
   return (
-    <div className="bg-blue-500 text-center h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-white">Hello, Tailwind!</h1>
-    </div>
+    <main className="bg-black">
+      <NavBar />
+      <Hero />
+      <Highlights />
+    </main>
   );
 };
 
